7533f5306e443668a001b6dcd70eee4d4ba1c747
Divya <divya.inapakurthi@gmail.com>
2025-03-14 10:27:19 -0400

    socket connection fix

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ function App() {
 
       { playOnline && !issocketConnected
          && (
-          <div className='flex justify-center flex-col gap-10 items-center h-full'>
+          <div className='flex p-2 justify-center flex-col gap-10 items-center h-full'>
 
             <div role="status">
               <svg aria-hidden="true" class="w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
